d07892b527aac4042978716594e5a452f5231302
illusion0001 <37698908+illusion0001@users.noreply.github.com>
2023-02-20 00:52:39 -0600

    Log Detour Hook creations

--- source/Utilities.c
+++ source/Utilities.c
@@ -11,22 +11,6 @@
 
 #include "Common.h"
 
-#ifdef __USE_PRINTF__
-#define klog printf
-#endif
-
-#ifdef __USE_KLOG__
-void klog(char* fmt, ...) {
-    // DO NOT INITIALIZE WITH 0!!
-    char Buffer[255];
-    va_list args;
-    va_start(args, fmt);
-    vsnprintf(Buffer, sizeof(Buffer), fmt, args);
-    va_end(args);
-    sys_sdk_cmd(GOLDHEN_SDK_CMD_KLOG, Buffer);
-}
-#endif
-
 void hex_dump(void *data, size_t size) {
     unsigned char *p = (unsigned char *)data;
     int i;
